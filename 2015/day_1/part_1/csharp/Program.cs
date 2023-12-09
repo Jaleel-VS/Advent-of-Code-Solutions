cc310568ce749297237a58ee986521ad7a938074
Jaleel <56233547+Jaleel-VS@users.noreply.github.com>
2023-12-09 09:42:47 +0000

    Update .gitignore and fix code logic

--- 2015/day_1/part_1/csharp/Program.cs
+++ 2015/day_1/part_1/csharp/Program.cs
@@ -1,12 +1,35 @@
-﻿var inputPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "input.txt");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var inputPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "input.txt");
 
-var inputData = File.ReadAllText(inputPath);
+        if (!File.Exists(inputPath))
+        {
+            Console.WriteLine("Input file not found.");
+            return;
+        }
 
-Dictionary<string, int> floorAdjustment = new Dictionary<string, int>();
+        try
+        {
+            var inputData = File.ReadAllText(inputPath);
+            var floorAdjustment = new Dictionary<char, int>
+            {
+                { '(', 1 },
+                { ')', -1 }
+            };
 
-floorAdjustment.add("(", 1)
-floorAdjustment.add(")", -1)
+            var currentFloor = 0;
+            foreach (var c in inputData)
+            {
+                currentFloor += floorAdjustment.GetValueOrDefault(c, 0);
+            }
 
-foreach(var floor in inputData)
-
-
+            Console.WriteLine($"The final floor is {currentFloor}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
