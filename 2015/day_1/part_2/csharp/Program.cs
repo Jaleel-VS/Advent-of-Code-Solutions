e8e937585d81c251635943192b414676dd913b71
Jaleel <56233547+Jaleel-VS@users.noreply.github.com>
2023-12-09 10:02:09 +0000

    Add C# code for solving Day 1 Part 2

---
+++ 2015/day_1/part_2/csharp/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var inputPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "input.txt");
+
+        // Check if the input file exists
+        if (!File.Exists(inputPath))
+        {
+            Console.WriteLine("Input file not found.");
+            return;
+        }
+
+        try
+        {
+            var inputData = File.ReadAllText(inputPath);
+            var floorAdjustment = new Dictionary<char, int>
+            {
+                { '(', 1 },
+                { ')', -1 }
+            };
+
+            var currentFloor = 0;
+
+            // Iterate through each character in the input data
+            for (var i = 0; i < inputData.Length; i++)
+            {
+                currentFloor += floorAdjustment.GetValueOrDefault(inputData[i], 0);
+
+                // Check if Santa has entered the basement
+                if (currentFloor < 0)
+                {
+                    Console.WriteLine($"The position of the character that causes Santa to first enter the basement is {i + 1}.");
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
