38590d394ab2e2c68e02cf4f3d83e75c6b979431
Jaleel <jeremikress@gmail.com>
2023-12-11 10:15:40 +0200

    Add getHousesVisited function to calculate the number of houses visited

--- 2015/day_3/part_1/csharp/Program.cs
+++ 2015/day_3/part_1/csharp/Program.cs
@@ -1,5 +1,44 @@
 class Program
 {
+    
+
+    static int getHousesVisited(char[] directions)
+    {
+        int x = 0; int y = 0;
+
+        HashSet<string> houses = new HashSet<string>
+        {
+            $"{x},{y}"
+        };
+
+        foreach (char direction in directions)
+        {
+            switch (direction)
+            {
+               
+                case '^':
+                    y++;
+                    break;
+                case 'v':
+                    y--;
+                    break;
+                case '>':
+                    x++;
+                    break;
+                case '<':
+                    x--;
+                    break;
+            }
+
+            houses.Add($"{x},{y}");
+            
+        }
+
+
+        return houses.Count;
+    }
+
+
     static void Main(string[] args)
     {
         string inputPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "input.txt");
@@ -10,6 +49,10 @@ class Program
             Console.WriteLine("Input file not found.");
             return;
         }
+
+        char[] directions = File.ReadAllText(inputPath).ToCharArray();
+
+        Console.WriteLine($"Houses visited: {getHousesVisited(directions)}");
     }
 }
     
