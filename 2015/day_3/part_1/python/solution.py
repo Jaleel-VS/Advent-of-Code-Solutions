029c6d4a13b3bd01e7646efd71510cdb9ddda90c
Jaleel <jeremikress@gmail.com>
2023-12-11 10:00:46 +0200

    Solve day 3 p1 in python

--- 2015/day_3/part_1/python/solution.py
+++ 2015/day_3/part_1/python/solution.py
@@ -1,7 +1,27 @@
 import os
 INPUT_PATH = None
 
+def get_houses_visited(input_file):
+    x, y = 0, 0
+    houses = set()
+    houses.add((x, y))
 
+    with open(input_file, "r") as f:
+        directions = f.read()
+
+    for direction in directions:
+        if direction == "^":
+            y += 1
+        elif direction == "v":
+            y -= 1
+        elif direction == ">":
+            x += 1
+        elif direction == "<":
+            x -= 1
+
+        houses.add((x, y))
+
+    return houses
 
 if __name__ == "__main__":
     INPUT_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "input.txt")
@@ -11,4 +31,8 @@ if __name__ == "__main__":
         print("Input file not found.")
         exit()
 
+    houses = get_houses_visited(INPUT_PATH)
+
+    print(f"Santa visited {len(houses)} houses.")
+    
     
