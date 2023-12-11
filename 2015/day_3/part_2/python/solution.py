97c113567f6239da09d4aa6136ccd6e068056b20
Jaleel <jeremikress@gmail.com>
2023-12-11 10:33:34 +0200

    Add function to calculate number of houses visited

--- 2015/day_3/part_2/python/solution.py
+++ 2015/day_3/part_2/python/solution.py
@@ -1,6 +1,35 @@
 import os
 INPUT_PATH = None
 
+def get_houses_visited(directions):
+    x, y, x1, y1 = 0, 0, 0, 0
+
+    houses = set()
+    houses.add((x, y))
+
+    for i, direction in enumerate(directions):
+        if i % 2 == 0:
+            if direction == "^":
+                y += 1
+            elif direction == "v":
+                y -= 1
+            elif direction == ">":
+                x += 1
+            elif direction == "<":
+                x -= 1
+            houses.add((x, y))
+        else:
+            if direction == "^":
+                y1 += 1
+            elif direction == "v":
+                y1 -= 1
+            elif direction == ">":
+                x1 += 1
+            elif direction == "<":
+                x1 -= 1
+            houses.add((x1, y1))
+
+    return len(houses)
 
 
 if __name__ == "__main__":
@@ -10,5 +39,10 @@ if __name__ == "__main__":
     if not os.path.exists(INPUT_PATH):
         print("Input file not found.")
         exit()
+    
+    with open(INPUT_PATH) as f:
+        directions = f.read().strip()
+
+    print(get_houses_visited(directions))
 
     
