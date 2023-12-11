3332477fc13b8db6b35e1f39f0462b1a425991d0
Jaleel <jeremikress@gmail.com>
2023-12-11 10:31:33 +0200

    Add logic to count the number of houses visited

--- 2015/day_3/part_2/csharp/Program.cs
+++ 2015/day_3/part_2/csharp/Program.cs
@@ -1,5 +1,8 @@
+
 class Program
 {
+
+
     static void Main(string[] args)
     {
         string inputPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "input.txt");
@@ -10,6 +13,68 @@ class Program
             Console.WriteLine("Input file not found.");
             return;
         }
+
+        char[] directions = File.ReadAllText(inputPath).ToCharArray();
+
+        Console.WriteLine($"Houses visited: {getHousesVisited(directions)}");
+    }
+
+    static int getHousesVisited(char[] directions)
+    {
+        int x = 0; int y = 0;
+        int x2 = 0; int y2 = 0;
+
+        HashSet<string> houses = new HashSet<string>
+         {
+              $"{x},{y}"
+         };
+
+         for (int i = 0; i < directions.Length; i++) {
+            if (i % 2 == 0) {
+                switch (directions[i])
+                {
+                    case '^':
+                        y++;
+                        break;
+                    case 'v':
+                        y--;
+                        break;
+                    case '>':
+                        x++;
+                        break;
+                    case '<':
+                        x--;
+                        break;
+                }
+
+                houses.Add($"{x},{y}");
+            } else {
+                switch (directions[i])
+                {
+                    case '^':
+                        y2++;
+                        break;
+                    case 'v':
+                        y2--;
+                        break;
+                    case '>':
+                        x2++;
+                        break;
+                    case '<':
+                        x2--;
+                        break;
+                }
+
+                houses.Add($"{x2},{y2}");
+            }
+         }
+
+
+
+
+
+
+        return houses.Count;
+
     }
 }
-    
