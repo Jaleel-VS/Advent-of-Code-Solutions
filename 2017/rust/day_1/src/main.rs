b521da44b624dc7e452b13c264180fc85084638d
Jaleel <jeremikress@gmail.com>
2023-12-21 15:39:43 +0200

    Add Rust solution for Day 1 puzzle

---
+++ 2017/rust/day_1/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs;
+
+fn main() {
+    let file_name = "input.txt";
+    let input = fs::read_to_string(file_name).expect("Error reading file");
+    let digits: Vec<u32> = input
+                            .chars()
+                            .map(|c| c.to_digit(10).expect("Not a digit"))
+                            .collect();
+
+    // solve part 1
+    let sum = solve_part_1(&digits);
+    println!("Part 1: {}", sum);
+
+
+
+}
+
+fn solve_part_1(digits: &Vec<u32>) -> u32 {
+    let mut sum = 0;
+    
+    for i in 0..digits.len() {
+        let next = (i + 1) % digits.len();
+        if digits[i] == digits[next] {
+            sum += digits[i];
+        }
+    }
+    sum
+}
