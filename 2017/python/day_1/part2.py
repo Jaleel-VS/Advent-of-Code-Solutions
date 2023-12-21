17d5a3bde4f5d5b72a5b0cd94c154e9239862ef8
Jaleel <jeremikress@gmail.com>
2023-12-21 15:55:06 +0200

    Add solution for Day 1 Part 2

---
+++ 2017/python/day_1/part2.py
@@ -0,0 +1,26 @@
+import os
+
+class Solution:
+    def __init__(self, input_file_path) -> None:
+        self.input_file_path = input_file_path
+        self.running_total = 0
+
+    def parse_input(self):
+        with open(self.input_file_path, "r") as input_file:
+            digits = input_file.read().strip()
+            length = len(digits)
+            for i in range(length):
+                if digits[i] == digits[(i + length // 2) % length]:
+                    self.running_total += int(digits[i])
+                    
+    def get_running_total(self):
+        return self.running_total
+                
+
+
+if __name__ == "__main__":
+    input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+    
+    sol = Solution(input_file_path)
+    sol.parse_input()
+    print(sol.get_running_total())
