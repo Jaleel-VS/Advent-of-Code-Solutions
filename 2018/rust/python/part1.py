565b352c3fe76199f54adecc43cbb9f0e2834549
Jaleel-VS <jeremikress@gmail.com>
2023-12-25 22:25:33 +0200

    Add Solution class to calculate total from input file

---
+++ 2018/rust/python/part1.py
@@ -0,0 +1,29 @@
+import os
+
+class Solution:
+    def __init__(self, input_file_path):
+        self.input_file_path = input_file_path
+
+        self.run()
+
+    def run(self):
+        with open(self.input_file_path) as f:
+            lines = f.readlines()
+
+        lines = [line.strip() for line in lines]
+        
+        total = 0
+
+        for line in lines:
+            sign, value = line[0], line[1:]
+
+            total += int(value) if sign == "+" else -int(value)
+
+        print(total)
+
+            
+
+
+if __name__ == "__main__":
+    input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+    solution = Solution(input_file_path)
